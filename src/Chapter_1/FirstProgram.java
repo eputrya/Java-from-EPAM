f542375860c1aa0c41446e352b01558b6ffb29f8
eugene <garden8b103@gmail.com>
2023-03-02 15:47:20 +0300

    add FirstProgram

---
+++ src/Chapter_1/FirstProgram.java
@@ -0,0 +1,10 @@
+package Chapter_1;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        // declaring and creating an object
+        TutorialAction action = new TutorialAction();
+        // calling a method that outputs a string
+        action.printMessage("tutorial-> https://docs.oracle.com/javase/tutorial/");
+    }
+}
---
+++ src/Chapter_1/HelloTutorial.java
@@ -0,0 +1,2 @@
+package Chapter_1;public class HelloTutorial {
+}
