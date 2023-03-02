2c209c0efc6197f46792ddd2f22d0af9ece955fd
eugene <garden8b103@gmail.com>
2023-03-02 15:48:09 +0300

    add TutorialAction

--- src/Chapter_1/TutorialAction.java
+++ src/Chapter_1/TutorialAction.java
@@ -1,2 +1,8 @@
-package Chapter_1;public class TutorialAction {
+package Chapter_1;
+
+public class TutorialAction {
+    void printMessage(String msg) { // method definition
+        // output string
+        System.out.println(msg);
+    }
 }
