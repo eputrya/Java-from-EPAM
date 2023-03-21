a92712e35ea9abb56a121d5c5373e2c0d1c98720
eugene <garden8b103@gmail.com>
2023-03-21 09:28:17 +0300

    add Course

---
+++ src/Chapter_2/Course.java
@@ -0,0 +1,16 @@
+package Chapter_2;
+
+/* # 2 # учебные курсы в учебном заведении: иерархия */
+public class Course {
+    void method() {
+    }
+}
+
+class BaseCourse extends Course { /* */
+}
+
+class FreeCourse extends BaseCourse { /* */
+}
+
+class OptionalCourse extends Course {/* */
+}
