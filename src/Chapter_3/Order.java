cfbec5a48326764763da121aeb860d0386414300
eugene <garden8b103@gmail.com>
2023-06-20 09:37:50 +0300

    add Order

---
+++ src/Chapter_3/Order.java
@@ -0,0 +1,25 @@
+package Chapter_3;
+
+public class Order {
+    // class instance variable
+    private long orderId;
+    // class/static variable
+    static int bonus;
+    // class instance final variable
+    public final int CURRENT_RANGE = (int) (Math.random() * 42);
+    // class instance final variable
+    private final MutableType mutable = new MutableType();
+    // class/static final variable
+    public final static int PURCHASE_TAX = 5;
+
+    // constructors
+    // methods
+    public double calculatePrice(double price, int counter) {
+        // local variable of the method does not get the default value
+        double amount;
+        // amount++; compile error, variable not initialized
+        amount = (price - bonus) * counter; // initialization of a local variable
+        double tax = amount * PURCHASE_TAX / 100;
+        return amount + tax;
+    }
+}
