a8a380d9b78b04ff4a99a3475baca5d247ac7885
eugene <garden8b103@gmail.com>
2023-07-03 09:23:40 +0300

    add Post

---
+++ src/Chapter_3/Post.java
@@ -0,0 +1,9 @@
+package Chapter_3;
+
+/* # 17 # объявление класса с двумя параметрами # Post.java */
+
+public class Post<K extends Number, V> {
+    private K postId;
+    private V message;
+    // fields & constructors & methods
+}
