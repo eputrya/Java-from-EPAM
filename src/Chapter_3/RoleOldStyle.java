8217c81b6349a40693fd8f9f66ebc4f788d022d7
eugene <garden8b103@gmail.com>
2023-07-04 10:03:17 +0300

    add RoleOldStyle

---
+++ src/Chapter_3/RoleOldStyle.java
@@ -0,0 +1,12 @@
+package Chapter_3;
+
+/* # 24 # объявление набора констант # RoleOldStyle.java */
+public class RoleOldStyle {
+    public final static int GUEST = 0;
+    public final static int CLIENT = 1;
+    public final static int MODERATOR = 2;
+    public final static int ADMIN = 3;
+
+    private RoleOldStyle() {
+    }
+}
