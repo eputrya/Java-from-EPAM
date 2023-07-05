e3ac2c16185bdf65d95027b8e9237aad9e9c17d4
eugene <garden8b103@gmail.com>
2023-07-05 14:45:31 +0300

    класс-record для создания неизменяемых объектов

---
+++ src/Chapter_3/ImmutableRec.java
@@ -0,0 +1,7 @@
+package Chapter_3;
+
+/* # 31 # класс-record для создания неизменяемых объектов # ImmutableRec.java */
+public record ImmutableRec(String name, int id) {
+    void method() {
+    }
+}
