5b7d7e66c9d2cffad1898f30f8ea9b690ee19507
eugene <garden8b103@gmail.com>
2023-07-19 12:54:32 +0300

    #9 super и this в конструкторе

---
+++ src/Chapter_4/Point1D.java
@@ -0,0 +1,32 @@
+package Chapter_4;
+
+// # 9 # super и this в конструкторе # Point1D.java # Point2D.java # Point3D.java
+public class Point1D {
+    private int x;
+
+    public Point1D(int x) {
+        this.x = x;
+    }
+}
+
+class Point2D extends Point1D {
+    private int y;
+
+    public Point2D(int x, int y) {
+        super(x);
+        this.y = y;
+    }
+}
+
+class Point3D extends Point2D {
+    private int z;
+
+    public Point3D(int x, int y, int z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public Point3D() {
+        this(-1, -1, -1); // call Point3D constructor with parameters
+    }
+}
