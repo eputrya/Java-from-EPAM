4fb3b88419c63a0e6b4e2ea97e59eb8ecf9c48d9
eugene <garden8b103@gmail.com>
2023-09-19 14:10:10 +0300

    /* # 17 # переопределение методов equals(), hashCode(), toString() # Student.java */

---
+++ src/Chapter_4/Student.java
@@ -0,0 +1,63 @@
+package Chapter_4;
+
+/* # 17 # переопределение методов equals(), hashCode(), toString() # Student.java */
+
+public class Student {
+    private int id;
+    private String name;
+    private int yearOfStudy;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int Id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYearOfStudy() {
+        return yearOfStudy;
+    }
+
+    public void setYearOfStudy(int yearOfStudy) {
+        this.yearOfStudy = yearOfStudy;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+
+        Student student = (Student) o;
+        if (id != student.id) {
+            return false;
+        }
+        if (yearOfStudy != student.yearOfStudy) {
+            return false;
+        }
+
+        return name != null ? name.equals(student.name) : student.name == null;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("Strudent{");
+        sb.append("id=").append(id);
+        sb.append(", name='").append(name).append('\'');
+        sb.append(", yearOfStudy=").append(yearOfStudy);
+        sb.append('}');
+        return sb.toString();
+    }
+}
