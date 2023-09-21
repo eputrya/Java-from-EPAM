deffead1908dddbc6d0f996ec54e5d2338097d07
eugene <garden8b103@gmail.com>
2023-09-21 14:08:05 +0300

    // # 24 # обращение к статическому методу и константе # ImportMain.java

---
+++ src/Chapter_4/ImportMain.java
@@ -0,0 +1,9 @@
+package Chapter_4;
+
+// # 24 # обращение к статическому методу и константе # ImportMain.java
+public class ImportMain {
+    public static void main(String[] args) {
+        System.out.println(2 * Math.PI * 3);
+        System.out.println(Math.floor(Math.cos(Math.PI / 3)));
+    }
+}
