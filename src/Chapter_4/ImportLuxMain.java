73b5a30fe3986d66fbec589baeddec1d67b390bb
eugene <garden8b103@gmail.com>
2023-09-21 14:10:15 +0300

    // # 25 # статический импорт методов и констант # ImportLuxMain.java

---
+++ src/Chapter_4/ImportLuxMain.java
@@ -0,0 +1,11 @@
+package Chapter_4;
+
+import static java.lang.Math.*;
+
+// # 25 # статический импорт методов и констант # ImportLuxMain.java
+public class ImportLuxMain {
+    public static void main(String[] args) {
+        System.out.println(2 * PI * 3);
+        System.out.println(floor(cos(PI / 3)));
+    }
+}
